15e7ef25a27099074fd372533d04c11cb2a6c482
Ethan Silverthorne <emsilver987@gmail.com>
2026-01-06 21:25:16 -0500

    added the ability to use lt and gt within the parameters

--- server.js
+++ server.js
@@ -14,13 +14,14 @@ const pool = new Pool({
   database: "weather_db",
 });
 
-// GET /cities?temp_lte=20&as_of=10m
+////////////////////////////////
+// GET /cities?temp_lt=20
+// GET /cities?temp_gt=80
+// GET /cities?temp_gt=32&temp_lt=50
+// GET /cities?temp_gt=32&temp_lt=50&as_of=15m
+////////////////////////////////
 app.get("/cities", async (request, reply) => {
-  const { temp_lte, as_of = "10m" } = request.query;
-
-  if (!temp_lte) {
-    return reply.code(400).send({ error: "temp_lte is required" });
-  }
+  const { temp_lt, temp_gt, as_of = "10m" } = request.query;
 
   // parse as_of like "10m"
   const minutes = parseInt(as_of.replace("m", ""), 10);
@@ -28,30 +29,57 @@ app.get("/cities", async (request, reply) => {
     return reply.code(400).send({ error: "invalid as_of format" });
   }
 
-  const result = await pool.query(
-    `
-    SELECT
-      c.name,
-      c.state,
-      w.temperature_f,
-      w.recorded_at
-    FROM cities c
-    JOIN LATERAL (
-      SELECT temperature_f, recorded_at
-      FROM weather_snapshots
-      WHERE city_id = c.id
-        AND recorded_at >= NOW() - INTERVAL '${minutes} minutes'
-      ORDER BY recorded_at DESC
-      LIMIT 1
-    ) w ON true
-    WHERE w.temperature_f <= $1
-    ORDER BY w.temperature_f ASC;
-    `,
-    [temp_lte]
-  );
+  const tempLt = temp_lt !== undefined ? Number(temp_lt) : null;
+  const tempGt = temp_gt !== undefined ? Number(temp_gt) : null;
 
+  if ((tempLt !== null && Number.isNaN(tempLt)) || tempGt !== null && Numebr.isNaN(tempGt)){
+    return reply.code(400).send({ error: "Temperature must be a number"})
+  }
+
+  
+  let tempConditions = [];
+  let params = [];
+  let paramIndex = 1;
+
+  if (tempGt !== null){
+    tempConditions.push(`w.temperature_f > $${paramIndex}`);
+    params.push(tempGt);
+    paramIndex++;
+  }
+
+  if (tempLt !== null){
+    tempConditions.push(`w.temperature_f < $${paramIndex}`);
+    params.push(tempLt);
+    paramIndex++;
+  }  
+
+  const tempWhereClause = tempConditions.length > 0 ? `AND ${tempConditions.join(" AND ")}` : "";
+
+  const query = `
+SELECT
+  c.name,
+  c.state,
+  w.temperature_f,
+  w.recorded_at
+FROM cities c
+JOIN LATERAL (
+  SELECT temperature_f, recorded_at
+  FROM weather_snapshots
+  WHERE city_id = c.id
+    AND recorded_at >= NOW() - INTERVAL '${minutes} minutes'
+  ORDER BY recorded_at DESC
+  LIMIT 1
+) w ON true
+WHERE 1=1
+${tempWhereClause}
+ORDER BY w.temperature_f ASC;
+`;
+
+  const result = await pool.query(query , params);
   return result.rows;
 });
+
+
 
 app.listen({ port: 3000, host: "0.0.0.0" });
 
